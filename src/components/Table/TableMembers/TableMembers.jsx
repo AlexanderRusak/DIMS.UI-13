a7ff2b242f256574aeb503fac71d932127a6b70d
Alexander Rusak <375298388602@mail.ru>
2021-04-05 23:39:05 +0300

    feat: add modal with backdrop but without syles

--- src/components/Table/TableMembers/TableMembers.jsx
+++ src/components/Table/TableMembers/TableMembers.jsx
@@ -1,50 +1,81 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { ModalCreateUpdate } from '../../Modal/ModalCreateUpdate';
 import { Button } from '../../UI/Buttons/Button/Button';
 import classes from '../Table.module.css';
 
-export const Table = (props) => {
-  const { data } = props;
-  const { UserId, FullName, Direction, Education, Age } = data;
-  const cls = [classes.button, classes.delete];
+export class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
 
-  return (
-    <>
-      <div className={classes.Table}>
-        <ul>
-          <li>
-            <p>{UserId}</p>
-          </li>
-          <li>
-            <p>{FullName}</p>
-          </li>
-          <li>
-            <p>{Direction}</p>
-          </li>
-          <li>
-            <p>{Education}</p>
-          </li>
-          <li className={classes.age}>
-            <p>{Age}</p>
-          </li>
-          <li className={classes.actions}>
-            <Button className={classes.button}>
-              <p className={classes.fontButton}>Progress</p>
-            </Button>
-            <Button className={classes.button}>
-              <p className={classes.fontButton}>Tasks</p>
-            </Button>
-            <Button className={classes.button}>
-              <p className={classes.fontButton}>Edit</p>
-            </Button>
-            <Button className={cls.join(' ')}>
-              <p className={classes.fontButton}>Delete</p>
-            </Button>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
-};
+  closeModalHandler = () => {
+    this.setState(() => {
+      return {
+        isOpen: false,
+      };
+    });
+  };
+
+  openModalHandler = () => {
+    const { isOpen } = this.state;
+
+    this.setState(() => {
+      return {
+        isOpen: !isOpen,
+      };
+    });
+  };
+
+  render() {
+    const { data } = this.props;
+    const { isOpen } = this.state;
+    const { UserId, FullName, Direction, Education, Age } = data;
+    const cls = [classes.button, classes.delete];
+
+    return (
+      <>
+        <div className={classes.Table}>
+          <ul>
+            <li>
+              <p>{UserId}</p>
+            </li>
+            <li>
+              <p>{FullName}</p>
+            </li>
+            <li>
+              <p>{Direction}</p>
+            </li>
+            <li>
+              <p>{Education}</p>
+            </li>
+            <li className={classes.age}>
+              <p>{Age}</p>
+            </li>
+            <li className={classes.actions}>
+              <Button className={classes.button}>
+                <p className={classes.fontButton}>Progress</p>
+              </Button>
+              <Button className={classes.button}>
+                <p className={classes.fontButton}>Tasks</p>
+              </Button>
+              <Button className={classes.button} onClick={this.openModalHandler}>
+                <p className={classes.fontButton}>Edit</p>
+              </Button>
+              <Button className={cls.join(' ')}>
+                <p className={classes.fontButton}>Delete</p>
+              </Button>
+            </li>
+          </ul>
+        </div>
+        <ModalCreateUpdate onClose={this.closeModalHandler} isOpen={isOpen} />
+      </>
+    );
+  }
+}
 
 Table.propTypes = {
   data: PropTypes.instanceOf(Object),
