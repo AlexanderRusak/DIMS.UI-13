d8149541ec883c071e813c21b1c909d3fc833832
AlexanderRusak <375298388602@mail.ru>
2021-04-14 23:56:18 +0300

    fix: remove comments

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-console.log(process.env.REACT_APP_API_KEY, 'react');
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -12,22 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-
-  /*   REACT_APP_API_KEY'AIzaSyClG2HvDOhiTZMfKCCDdBhAVKolO0uj-e8'
-        AUTH_DOMAIN='dims-13.firebaseapp.com'
-PROJECT_ID='dims-13'
-STORAGE_BUCKET='dims-13.appspot.com'
-MESSAGING_SENDER_ID= '1053601047185'
-APP_ID='1:1053601047185:web:08b457cfa61944a1bb9530'
-MEASUREMENT_ID='G-0Z06LEVEPN' */
-
-  /*   apiKey: 'AIzaSyClG2HvDOhiTZMfKCCDdBhAVKolO0uj-e8',
-    authDomain: 'dims-13.firebaseapp.com',
-    projectId: 'dims-13',
-    storageBucket: 'dims-13.appspot.com',
-    messagingSenderId: '1053601047185',
-    appId: '1:1053601047185:web:08b457cfa61944a1bb9530',
-    measurementId: 'G-0Z06LEVEPN', */
 };
 
 firebase.initializeApp(firebaseConfig);
