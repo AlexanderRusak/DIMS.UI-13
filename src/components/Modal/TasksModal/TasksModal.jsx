a0c8e4554c0f0ea16ac4cb77ca3dc3bb21708265
AlexanderRusak <375298388602@mail.ru>
2021-04-28 16:42:36 +0300

    feat: strt doing validation

--- src/components/Modal/TasksModal/TasksModal.jsx
+++ src/components/Modal/TasksModal/TasksModal.jsx
@@ -6,76 +6,95 @@ import { Input } from '../../UI/Input/Input';
 import { toLowerCaseFirstLetter, toTrim } from '../modalHelpers/helpers';
 import classes from './TasksModal.module.css';
 import { Label } from '../../UI/Label/Label';
+import { setMinLengthRequired, isCheckBoxValueRequired } from '../../Validation/validationHelpers';
 
 export class TasksModal extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      test: 'rerre',
       users: [...props.users],
-      userList: props.users.map((user) => { return { name: user.name, isCheck: false } }),
+      userList: props.users.map((user) => {
+        return { name: user.name, isCheck: false };
+      }),
       taskName: props.type === 'create' ? '' : props.data[props.index].taskName,
       description: props.type === 'create' ? '' : props.data[props.index].description,
       startDate: props.type === 'create' ? '' : props.data[props.index].startDate,
       deadLine: props.type === 'create' ? '' : props.data[props.index].deadLine,
+      touched: {
+        taskName: false,
+        description: false,
+        startDate: false,
+        deadLine: false,
+        checkbox: false,
+      },
     };
   }
+
+  setTouched = (elementName) => {
+    const { touched } = this.state;
+    const newTouched = { ...touched, [elementName]: true };
+    this.setState({ touched: newTouched });
+  };
 
   setCheckedUser = (index) => {
     const { users } = this.state;
     const newUsers = [...users];
     newUsers[index].isCheck = !users[index].isCheck;
-    this.setState({ users: newUsers })
+    this.setState({ users: newUsers });
   };
 
   setCreateCheckedUser = (index) => {
     const { userList } = this.state;
     const newUsers = [...userList];
     newUsers[index].isCheck = !userList[index].isCheck;
-    this.setState({ userList: newUsers })
+    console.log(isCheckBoxValueRequired(newUsers));
+    this.setTouched();
+    this.setState({ userList: newUsers });
   };
 
   getUserDataArray = () => {
-    const { taskName, deadLine, description, startDate, test } = this.state;
+    const { taskName, deadLine, description, startDate, touched } = this.state;
     return [
-      { value: taskName, title: 'Task Name', isValid: test },
-      { value: description, title: 'Description', isValid: test },
-      { value: startDate, title: 'Start Date', isValid: test },
-      { value: deadLine, title: 'Dead Line', isValid: test },
-    ]
-  }
+      { value: taskName, title: 'Task Name', isValid: !!setMinLengthRequired(taskName, 5) || !touched.taskName },
+      {
+        value: description,
+        title: 'Description',
+        isValid: !!setMinLengthRequired(taskName, 5) || !touched.description,
+      },
+      { value: startDate, title: 'Start Date', inputType: 'date', isValid: startDate || !touched.startDate },
+      { value: deadLine, title: 'Dead Line', inputType: 'date', isValid: startDate || !touched.startDate },
+    ];
+  };
 
   getInputValue = (event) => {
     const elementName = toLowerCaseFirstLetter(toTrim(event.target.attributes[1].nodeValue));
+    this.setTouched(elementName);
     this.setState({
-      [elementName]: event.target.value
-    })
-  }
+      [elementName]: event.target.value,
+    });
+  };
 
   onSubmitHandler = (data, users, index, type) => {
     const { onSubmit } = this.props;
     onSubmit(data, users, index, type);
-  }
+  };
 
   onCloseHandler = () => {
     const { onClose } = this.props;
-/*     this.setState({ touched: false }) */
     onClose();
-  }
+  };
 
   renderItems = () => {
-
     const { type } = this.props;
     const currentUsers = this.getUserDataArray();
-    return currentUsers.map(({ value, title, isValid }) =>
-      type === 'details' ?
-        <Label value={value} title={title} /> :
-        <Input value={value} title={title} isValid={!!isValid} onChange={this.getInputValue} />
-    )
-  }
-
-
-
+    return currentUsers.map(({ value, title, isValid, inputType }) =>
+      type === 'details' ? (
+        <Label value={value} title={title} />
+      ) : (
+        <Input value={value} title={title} isValid={!!isValid} type={inputType} onChange={this.getInputValue} />
+      ),
+    );
+  };
 
   getModal = (index, type) => {
     const { taskName, deadLine, description, startDate, userList, users } = this.state;
@@ -85,15 +104,25 @@ export class TasksModal extends PureComponent {
     return (
       <div className={classes.container}>
         {this.renderItems()}
-        {type === 'details' ?
+        {type === 'details' ? (
           <div className={classes.selectedUsers}>
             {detailsUsers.map((user) => (
               <Label key={user.name} value={user.name} />
             ))}
           </div>
-          : <Checkbox checkHandler={type === 'create' ? this.setCreateCheckedUser : this.setCheckedUser} users={currentUsers} />}
+        ) : (
+          <Checkbox
+            checkHandler={type === 'create' ? this.setCreateCheckedUser : this.setCheckedUser}
+            users={currentUsers}
+          />
+        )}
         <div className={classes.btnGroup}>
-          <Button typeButton='primary' onClick={() => this.onSubmitHandler({ taskName, deadLine, description, startDate }, currentUsers, index, type)}>
+          <Button
+            typeButton='primary'
+            onClick={() =>
+              this.onSubmitHandler({ taskName, deadLine, description, startDate }, currentUsers, index, type)
+            }
+          >
             Create
           </Button>
           <Button typeButton='default' onClick={this.onCloseHandler}>
@@ -102,16 +131,12 @@ export class TasksModal extends PureComponent {
         </div>
       </div>
     );
-  }
+  };
 
   render() {
     const { type, index } = this.props;
 
-    return (
-      <div className={classes.TasksModal}>
-        { this.getModal(index, type)}
-      </div>
-    );
+    return <div className={classes.TasksModal}>{this.getModal(index, type)}</div>;
   }
 }
 
@@ -125,5 +150,5 @@ TasksModal.propTypes = {
 };
 TasksModal.defaultProps = {
   index: null,
-  data: null
+  data: null,
 };
--- src/components/Validation/validationHelpers.jsx
+++ src/components/Validation/validationHelpers.jsx
@@ -1,1 +1,12 @@
-export const 
+export const isValidEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+export const setMinLengthRequired = (fieldString, reqLength) => {
+  return fieldString.length >= reqLength;
+};
+
+export const isCheckBoxValueRequired = (lists) => {
+  return !!Object.values(lists).find((list) => list.isCheck);
+};
