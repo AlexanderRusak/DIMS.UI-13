7c421e2389ea7a31a51d4dc8aafa0a3572fa9b25
AlexanderRusak <375298388602@mail.ru>
2021-05-03 21:21:19 +0300

    test: rewrite tests with parameters

--- src/components/Auth/Auth.test.js
+++ src/components/Auth/Auth.test.js
@@ -3,13 +3,17 @@ import { isValidEmail, isValidMinLength, validateRequired } from './helpers';
 
 describe(validateControl, () => {
 
+    const actual = 'fake@mail.com';
+    const data = { required: true, email: true }
+    const fake = 'fake@mail';
+
     test('Should return value isValid===true', () => {
-        expect(validateControl('fake@mail.com', { required: true, email: true })).toBe(true);
-        expect(validateControl('fake@mail.com', { required: true, email: true })).toBeTruthy()
+        expect(validateControl(actual, data)).toBe(true);
+        expect(validateControl(actual, data)).toBeTruthy()
     })
     test('Should return value isValid===false', () => {
-        expect(validateControl('fake@mail', { required: true, email: true })).toBe(false);
-        expect(validateControl('fake@mail', { required: true, email: true })).toBeFalsy()
+        expect(validateControl(fake, { required: true, email: true })).toBe(false);
+        expect(validateControl(fake, { required: true, email: true })).toBeFalsy()
     })
     test('Should has value and validation arguments', () => {
         expect(validateControl.length).toBe(2);
@@ -18,36 +22,46 @@ describe(validateControl, () => {
         expect(validateControl.length).not.toBe(3);
     })
     test('Should has undefined for non validation', () => {
-        expect(validateControl('fake@mail', undefined)).toBe(false);
+        expect(validateControl(fake, undefined)).toBe(false);
     })
 
 })
 
 describe(isValidMinLength, () => {
+
+    const actual = 'string';
+    const fake = 'string   ';
+
     test('Should return true', () => {
-        expect(isValidMinLength('123456', 6)).toBe(true);
-        
+        expect(isValidMinLength(actual, 6)).toBe(true);
+
     })
     test('Should return false', () => {
-        expect(isValidMinLength('12345', 6)).toBe(false);
+        expect(isValidMinLength(fake, 15)).toBe(false);
     })
-    
+
 })
 
 describe(isValidEmail, () => {
-    test('Should return valid email', () => {
-        expect(isValidEmail('test@mail.ru')).toBe(true);
+
+    const actual = 'test@mail.ru';
+    const fake = 'test@mail';
+
+    test('Should return true if valid email', () => {
+        expect(isValidEmail(actual)).toBe(true);
     })
-    test('Should return unvalid email', () => {
-        expect(isValidEmail('test@mail')).toBe(false);
-        expect(isValidEmail('testmail.ru')).toBe(false);
+    test('Should return false if invalid email', () => {
+        expect(isValidEmail(fake)).toBe(false);
     })
 })
 describe(validateRequired, () => {
+    const actual = 'string     ';
+    const exceded = actual.trim().length;
+
     test('Should return string without spaces', () => {
-        expect(validateRequired('Word   ').length).toBe(4);
+        expect(validateRequired(actual).length).toBe(exceded);
     })
     test('Should return string without spaces', () => {
-        expect(validateRequired('Word   ').length).not.toBe(5);
+        expect(validateRequired(actual).length).not.toBe(actual);
     })
 })
